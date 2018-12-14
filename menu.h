a7c90cfb65775f35cd6a23a5f1452423a868bb7e
Mceresole <45624502+Mceresole@users.noreply.github.com>
2018-12-14 15:16:26 +0100

    Add files via upload

--- menu.h
+++ menu.h
@@ -1,7 +1,18 @@
-#include "carte.h"
-#define NBACQUI 23
-#define MAX 1000
-#define NBCOURRIER 80
-#define TAILLE 120
+#ifndef _MENU_H_
+#define _MENU_H_
+#include "joueur.h"
+#include "sauvegarde.h"
+#include "courrier.h"
+#include "acquisition.h"
 
-int menu(joueur *J);
+
+int intialiser(int *nb_tour, int *tour, courrier listeC[], acquisition listeA[], joueur listeJ[], int *nJ);
+
+/* initialise la partie avec choix de prendre une sauvegarde ou une nouvelle partie, et si c'est une nouvelle partie qui est choisit, demande du nombre de tours, de joueurs, etc. Cette fontion prend en paramètre, le nombre de tour, le numéro du tour en court, la liste des courrier, la liste des acquisitions, la liste des joueurs et ne numéro du joueur qui doit jouer (chacun de ses paramètre pour les modifier et les initialiser) */
+
+
+int jouer();
+
+/* le jeu */
+
+#endif
