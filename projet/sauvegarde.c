846af8eaaa5faa9888e653b4e73d56dafaa6925c
Mceresole <45624502+Mceresole@users.noreply.github.com>
2018-12-14 11:05:27 +0100

    Add files via upload

--- projet/sauvegarde.c
+++ projet/sauvegarde.c
@@ -1,3 +1,5 @@
+#ifndef _SAUVEGARDE_C_
+#define _SAUVEGARDE_C_
 #include "joueur.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,7 +7,7 @@
 
 /* Attention, lorsque encrypter est utiliser, il faut vérifier la valeur de retour, pour savoir si la sauvegarde à bien était crée*/
 
-int encrypter(joueur j, joueur listeJ[], int cagnotte, int tour, int nb_tour){
+int encrypter(joueur j, joueur listeJ[], int cagnotte, int tour, int nb_tour, courrier listeC[], acquisition listeA[]){
   int i, k;
   char s[MAX];
   FILE * f;
@@ -24,13 +26,17 @@ int encrypter(joueur j, joueur listeJ[], int cagnotte, int tour, int nb_tour){
     for(k = 0 ; k < listeJ[i].nb_acquisition ; k++)
       fprintf(f, "%s %d %d %d %d\n", listeJ[i].sesAcquisitions[k].titre, listeJ[i].sesAcquisitions[k].achat, listeJ[i].sesAcquisitions[k].vente, listeJ[i].sesAcquisitions[k].commission, listeJ[i].sesAcquisitions[k].valeur);
   }
+  for(i = 0 ; i < NBCOURRIER ; i++)
+    fprintf(f, "%s %d %d %d\n", listeC[i].message, listeC[i].somme, listeC[i].valeur, listeC[i].numero);
+  for(i = 0 ; i < NBACQUI ; i++)
+    fprintf(f, "%s %d %d %d %d\n", listeA[i].titre, listeA[i].achat, listeA[i].vente, listeA[i].commission, listeA[i].valeur);
   return 1;
 }
 
 /* Ne pas oublier d'ouvrir le ficher avant de l'utiliser !!!! */
-int decrypter(FILE *f,joueur j, joueur listeJ[], int cagnotte, int tour, int nb_tour){
+int decrypter(FILE *f, int *nJ, joueur listeJ[], int *cagnotte, int *tour, int *nb_tour, courrier listeC[], acquisition listeA[]){
   int i, k;
-  fscanf(f, "%d %d %d %d %d", &tour, &nb_tour, &j.numJ, &listeJ[0].numJ, &cagnotte);
+  fscanf(f, "%d %d %d %d %d", tour, nb_tour, nJ, &listeJ[0].numJ, cagnotte);
   for(i = 1; i < listeJ[0].numJ ; i++){
     fscanf(f, "%s %d %d %d %d %d %d %d %d", listeJ[i].Joueur, &listeJ[i].numJ, &listeJ[i].total, &listeJ[i].c, &listeJ[i].pret, &listeJ[i].epargne, &listeJ[i].type, &listeJ[i].nb_courrier, &listeJ[i].nb_acquisition);
     for(k = 0 ; k < listeJ[i].nb_courrier ; k++)
@@ -38,6 +44,11 @@ int decrypter(FILE *f,joueur j, joueur listeJ[], int cagnotte, int tour, int nb_
     for(k = 0 ; k < listeJ[i].nb_acquisition ; k++)
       fscanf(f, "%s %d %d %d %d", listeJ[i].sesAcquisitions[k].titre, &listeJ[i].sesAcquisitions[k].achat, &listeJ[i].sesAcquisitions[k].vente, &listeJ[i].sesAcquisitions[k].commission, &listeJ[i].sesAcquisitions[k].valeur);
   }
+  for(i = 0 ; i < NBCOURRIER ; i++)
+    fscanf(f, "%s %d %d %d", listeC[i].message, &listeC[i].somme, &listeC[i].valeur, &listeC[i].numero);
+  for(i = 0 ; i < NBACQUI ; i++)
+    fscanf(f, "%s %d %d %d %d", listeA[i].titre, &listeA[i].achat, &listeA[i].vente, &listeA[i].commission, &listeA[i].valeur);
   return 1;
 }
 
+#endif
