7ac668cf8951b2b8bdadd2642264f9eb3d918b4f
Mceresole <45624502+Mceresole@users.noreply.github.com>
2018-12-16 11:27:19 +0100

    modif faite

--- sauvegarde.h
+++ sauvegarde.h
@@ -2,8 +2,8 @@
 #define _SAUVEGARDE_H_
 #include "joueur.h"
 
-int encrypter(joueur j, joueur listeJ[], int cagnotte, int tour, int nb_tour, courrier listeC[], acquisition listeA[]);
+int encrypter(joueur j, joueur listeJ[], int cagnotte, int liste_mois, int nb_tour, courrier listeC[], acquisition listeA[]);
 
-int decrypter(FILE *f, int *nJ, joueur listeJ[], int *cagnotte, int *tour, int *nb_tour, courrier listeC[], acquisition listeA[]);
+int decrypter(FILE *f, int *nJ, joueur listeJ[], int *cagnotte, int liste_mois, int *nb_tour, courrier listeC[], acquisition listeA[]);
 
 #endif
