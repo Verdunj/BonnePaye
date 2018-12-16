1d0a9e42a9c8095171f92405a47671ff67d3877b
Mceresole <45624502+Mceresole@users.noreply.github.com>
2018-12-16 17:21:55 +0100

    commenté

--- livret_epargne.h
+++ livret_epargne.h
@@ -3,17 +3,17 @@
 
 #include "joueur.h"
 
-int interer(int montant);
+int interer(int montant); /* détermine les intérêts en fonction du montant entré en paramètre */
 
-void livret(joueur *j);
+void livret(joueur *j); /* donne au joueur en paramètre les intérêts perçus */
 
-void depose_joueur(joueur *j);
+void depose_joueur(joueur *j); /* propose (pour les joueurs autres que ordinateurs) de déposer de l'argent sur son épargne */
 
-void depose_ordi(joueur *j);
+void depose_ordi(joueur *j); /* s'exécute pour les ordinateurs et dépose 5% du total du joueur */
 
-void retire_argent_j(joueur *j);
+void retire_argent_j(joueur *j); /* propose à un joueur autre que ordinateur de retirer de l'argent */
 
-void retire_argent_o(joueur * j);
+void retire_argent_o(joueur * j); /* pour les ordinateurs, retire 400€ du livret d'épargne, seulement si le joueur à moins de 200€ et plus de 400€ sur son livret d'épargne */
 
 
 
