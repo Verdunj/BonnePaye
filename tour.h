82a34e5f5e3de88180e0a0c95823e408b3173198
Mceresole <45624502+Mceresole@users.noreply.github.com>
2018-12-16 17:43:12 +0100

    commenté

--- tour.h
+++ tour.h
@@ -6,8 +6,8 @@
 #include "plateau.h"
 #include "aff_plateau.h"
 
-int lance_des(joueur *j, int *cagnotte);
+int lance_des(joueur *j, int *cagnotte); /* réalise un lancer de dés et dépalce le joueur */
 
-void jouer_tour(int nJ, joueur listeJ[], courrier listeC[],acquisition listeA[],evenement listeE[],int *cagnotte, int liste_mois[], int nb_mois);
+void jouer_tour(int nJ, joueur listeJ[], courrier listeC[],acquisition listeA[],evenement listeE[],int *cagnotte, int liste_mois[], int nb_mois); /* réalise un tour de jouer pour une partie (lancer de dés, déplacement, exécution de ce qu'il se passe en fonction de la case, proposition d'épargner, si les conditons sont respéctées, et de retirer de l'argent) */
 
 #endif
